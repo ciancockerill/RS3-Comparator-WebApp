c5305eb299097f8f2bf7ea90bf734433d3884f0d
ciancockerill <100618890+ciancockerill@users.noreply.github.com>
2024-08-29 00:32:37 +0100

    Main logo url redirect

--- src/components/SideNavBar.tsx
+++ src/components/SideNavBar.tsx
@@ -5,7 +5,11 @@ import '../styling/SideNavBar.css'
 function SideNavBar() {
     return (
         <div className="sidenav list-group list-group-flush">
-            <img src={logo} alt="logo" className="logo" />
+
+            <a className="head-img" href="https://runescape.wiki/w/Application_programming_interface">
+                <img src={logo} alt="runescape head" className="logo"/>
+            </a>
+
             <Link to="/" className="list-group-item list-group-item-action">Home</Link>
             <Link to="/playersearch" className="list-group-item list-group-item-action">Single Player Search</Link>
             <Link to="/playercomparison" className="list-group-item list-group-item-action">Player Comparison</Link>
